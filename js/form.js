5f2f7a7297d37001d1a1495a1eda7442cc1969fb
Elizaveta <eljobemail@gmail.com>
2017-02-02 11:24:44 +0300

    Добавила файл form.js

---
+++ js/form.js
@@ -0,0 +1,72 @@
+'use strict';
+var pins = document.querySelectorAll('.pin');
+var dialog = document.querySelector('.dialog');
+var dialogClose = document.querySelector('.dialog__close');
+
+var removeClass = function(pins, className) {
+  for(var j = 0; j < pins.length; j++) {
+    pins[j].classList.remove(className);
+  }
+};
+
+for(var i = 0; i < pins.length; i++) {
+  pins[i].addEventListener('click', function(){
+    removeClass(pins, 'pin--active');
+    this.classList.add('pin--active');
+    dialog.style.display = 'block';
+  });
+}
+
+dialogClose.addEventListener('click', function(){
+  dialog.style.display = 'none';
+  removeClass(pins, 'pin--active');
+});
+
+var time = document.querySelector('#time');
+var timeout = document.querySelector('#timeout');
+
+var synchronizeTime = function(time, timeout) {
+  time.addEventListener('click', function() {
+    var timeoutIndex = time.options.selectedIndex;
+    timeout.options[timeoutIndex].selected = true;
+  });
+}
+
+synchronizeTime(time, timeout);
+synchronizeTime(timeout, time);
+
+var type = document.querySelector('#type');
+var price = document.querySelector('#price');
+
+type.addEventListener('click', function() {
+  var typeSelectedText = type.options[type.options.selectedIndex].text;
+
+  if (typeSelectedText === 'Квартира') {
+    price.setAttribute('min', '1000');
+    price.placeholder = '1000';
+  }
+  if (typeSelectedText === 'Лачуга') {
+    price.setAttribute('min', '0');
+    price.placeholder = '0';
+  }
+  if (typeSelectedText === 'Дворец') {
+    price.setAttribute('min', '10000');
+    price.placeholder = '10000';
+  }
+});
+
+var roomNumber = document.querySelector('#room_number');
+var capacity = document.querySelector('#capacity');
+
+roomNumber.addEventListener('click', function() {
+    var numberOfSelectedRoom = roomNumber.options[roomNumber.selectedIndex].value;
+
+    if (numberOfSelectedRoom === '1') {
+      capacity.querySelector('[value="0"]').selected = true;
+    }
+    if(numberOfSelectedRoom === '2' || numberOfSelectedRoom === '100') {
+      capacity.querySelector('[value="3"]').selected = true;
+    }
+});
+
+
