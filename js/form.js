c5c96ca99fbcd14ade3ba496e7e5662de855a392
Elizaveta <eljobemail@gmail.com>
2017-02-13 15:39:20 +0300

    Разделила функцирнальность на модули

--- js/form.js
+++ js/form.js
@@ -1,104 +1,25 @@
 'use strict';
-var pins = document.querySelectorAll('.pin');
-var pinMap = document.querySelector('.tokyo__pin-map');
-var dialog = document.querySelector('.dialog');
-var dialogClose = document.querySelector('.dialog__close');
 
-var ENTER_KEY_CODE = 13;
+var pinsEl = document.querySelectorAll('.pin');
+var pinMapEl = document.querySelector('.tokyo__pin-map');
+var dialogEl = document.querySelector('.dialog');
+var dialogCloseEl = document.querySelector('.dialog__close');
 
-var isActivateEvent = function (evt) {
-  return evt.keyCode && evt.keyCode === ENTER_KEY_CODE;
-};
-
-var removeClass = function (pinsElements, className) {
-  for (var j = 0; j < pinsElements.length; j++) {
-    pinsElements[j].classList.remove(className);
-    pinsElements[j].setAttribute('aria-pressed', 'false');
-  }
-};
-
-var showDialog = function (targetPinElement) {
-  removeClass(pins, 'pin--active');
-  targetPinElement.classList.add('pin--active');
-  targetPinElement.setAttribute('aria-pressed', 'true');
-  dialog.style.display = 'block';
-  dialog.setAttribute('aria-hidden', 'false');
-};
-
-var hideDialog = function () {
-  dialog.style.display = 'none';
-  removeClass(pins, 'pin--active');
-  dialog.setAttribute('aria-hidden', 'true');
-};
-
-pinMap.addEventListener('click', function (evt) {
-  var targetPin = evt.target.parentNode;
-  if (targetPin.classList.contains('pin')) {
-    showDialog(evt.target.parentNode);
-  }
-});
-
-pinMap.addEventListener('keydown', function (evt) {
-  var targetPin = evt.target;
-  if (targetPin.classList.contains('pin') && isActivateEvent(evt)) {
-    showDialog(evt.target);
-  }
-});
-
-dialogClose.addEventListener('click', function () {
-  hideDialog();
-});
-
-dialogClose.addEventListener('keydown', function (evt) {
-  if (isActivateEvent(evt)) {
-    hideDialog();
-  }
-});
+window.initializePins(pinsEl, pinMapEl, dialogEl, dialogCloseEl);
 
 var time = document.querySelector('#time');
 var timeout = document.querySelector('#timeout');
 
-var synchronizeTime = function (timeElement, timeoutElement) {
-  timeElement.addEventListener('click', function () {
-    var timeoutIndex = timeElement.options.selectedIndex;
-    timeoutElement.options[timeoutIndex].selected = true;
-  });
-};
-
-synchronizeTime(time, timeout);
-synchronizeTime(timeout, time);
+window.synchronizeFields(time, timeout, [], [], 'index');
 
 var type = document.querySelector('#type');
 var price = document.querySelector('#price');
 
-type.addEventListener('click', function () {
-  var typeSelectedText = type.options[type.options.selectedIndex].text;
-
-  if (typeSelectedText === 'Квартира') {
-    price.setAttribute('min', '1000');
-    price.placeholder = '1000';
-  }
-  if (typeSelectedText === 'Лачуга') {
-    price.setAttribute('min', '0');
-    price.placeholder = '0';
-  }
-  if (typeSelectedText === 'Дворец') {
-    price.setAttribute('min', '10000');
-    price.placeholder = '10000';
-  }
-});
+window.synchronizeFields(type, price, ['Квартира', 'Лачуга', 'Дворец'], ['1000', '0', '10000'], 'min');
 
 var roomNumber = document.querySelector('#room_number');
 var capacity = document.querySelector('#capacity');
 
-roomNumber.addEventListener('click', function () {
-  var numberOfSelectedRoom = roomNumber.options[roomNumber.selectedIndex].value;
-  if (numberOfSelectedRoom === '1') {
-    capacity.querySelector('[value="0"]').selected = true;
-  }
-  if (numberOfSelectedRoom === '2' || numberOfSelectedRoom === '100') {
-    capacity.querySelector('[value="3"]').selected = true;
-  }
-});
+window.synchronizeFields(roomNumber, capacity, ['1', '2', '100'], [0, 3], 'value');
 
 
---
+++ js/synchronize-fields.js
@@ -0,0 +1,47 @@
+'use strict';
+
+window.synchronizeFields = function (firstField, secondField, firstValuesArr, secondValuesArr, property) {
+  var synchronizeTime = function (timeElement, timeoutElement) {
+    timeElement.addEventListener('click', function () {
+      var timeoutIndex = timeElement.options.selectedIndex;
+      timeoutElement.options[timeoutIndex].selected = true;
+    });
+  };
+
+  if (property === 'index') {
+    synchronizeTime(firstField, secondField);
+    synchronizeTime(secondField, firstField);
+  }
+
+  if (property === 'min') {
+    firstField.addEventListener('click', function () {
+      var typeSelectedText = firstField.options[firstField.options.selectedIndex].text;
+
+      if (typeSelectedText === firstValuesArr[0]) {
+        secondField.setAttribute('min', secondValuesArr[0]);
+        secondField.placeholder = secondValuesArr[0];
+      }
+      if (typeSelectedText === firstValuesArr[1]) {
+        secondField.setAttribute('min', secondValuesArr[1]);
+        secondField.placeholder = secondValuesArr[1];
+      }
+      if (typeSelectedText === firstValuesArr[2]) {
+        secondField.setAttribute('min', secondValuesArr[2]);
+        secondField.placeholder = secondValuesArr[2];
+      }
+    });
+  }
+
+  if (property === 'value') {
+    firstField.addEventListener('click', function () {
+      var numberOfSelectedRoom = firstField.options[firstField.options.selectedIndex].value;
+
+      if (numberOfSelectedRoom === firstValuesArr[0]) {
+        secondField.querySelector('[value="' + secondValuesArr[0] + '"]').selected = true;
+      }
+      if (numberOfSelectedRoom === firstValuesArr[1] || numberOfSelectedRoom === firstValuesArr[2]) {
+        secondField.querySelector('[value="' + secondValuesArr[1] + '"]').selected = true;
+      }
+    });
+  }
+};
