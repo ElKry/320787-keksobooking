ae7d0fde9369763df1e4b1ecd5379b7e2a4542fc
Elizaveta <eljobemail@gmail.com>
2017-02-15 17:00:13 +0300

    Сделала синхронизацию модулей

--- js/form.js
+++ js/form.js
@@ -5,21 +5,21 @@ var pinMapEl = document.querySelector('.tokyo__pin-map');
 var dialogEl = document.querySelector('.dialog');
 var dialogCloseEl = document.querySelector('.dialog__close');
 
-window.initializePins(pinsEl, pinMapEl, dialogEl, dialogCloseEl);
+window.initPins.initializePins(pinsEl, pinMapEl, dialogEl, dialogCloseEl);
 
 var time = document.querySelector('#time');
 var timeout = document.querySelector('#timeout');
 
-window.synchronizeFields(time, timeout, [], [], 'index');
+window.syncFields.synchronizeFields(time, timeout, [], [], 'index');
 
 var type = document.querySelector('#type');
 var price = document.querySelector('#price');
 
-window.synchronizeFields(type, price, ['Квартира', 'Лачуга', 'Дворец'], ['1000', '0', '10000'], 'min');
+window.syncFields.synchronizeFields(type, price, ['Квартира', 'Лачуга', 'Дворец'], ['1000', '0', '10000'], 'min');
 
 var roomNumber = document.querySelector('#room_number');
 var capacity = document.querySelector('#capacity');
 
-window.synchronizeFields(roomNumber, capacity, ['1', '2', '100'], [0, 3], 'value');
+window.syncFields.synchronizeFields(roomNumber, capacity, ['1', '2', '100'], [0, 3], 'value');
 
 
