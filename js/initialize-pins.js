2a0ebfad70dd80131157ebc71ae6cc6b50f37474
Elizaveta <eljobemail@gmail.com>
2017-02-18 13:35:08 +0300

    Сделала коллбэки

--- js/initialize-pins.js
+++ js/initialize-pins.js
@@ -1,57 +1,24 @@
 'use strict';
 
-window.initPins = (function () {
-  var ENTER_KEY_CODE = 13;
-
-  var isActivateEvent = function (evt) {
-    return evt.keyCode && evt.keyCode === ENTER_KEY_CODE;
-  };
-
-  var removeClass = function (pinsElements, className) {
-    for (var j = 0; j < pinsElements.length; j++) {
-      pinsElements[j].classList.remove(className);
-      pinsElements[j].setAttribute('aria-pressed', 'false');
-    }
-  };
-
-  var showDialog = function (targetPinElement, pins, dialog) {
-    removeClass(pins, 'pin--active');
-    targetPinElement.classList.add('pin--active');
-    targetPinElement.setAttribute('aria-pressed', 'true');
-    dialog.style.display = 'block';
-    dialog.setAttribute('aria-hidden', 'false');
-  };
-
-  var hideDialog = function (pins, dialog) {
+window.hideCard = (function () {
+  var hideDialog = function (pins, dialog, cb) {
     dialog.style.display = 'none';
-    removeClass(pins, 'pin--active');
+    if (typeof cb === 'function') {
+      cb();
+    }
+    window.utils.removeClass(pins, 'pin--active');
     dialog.setAttribute('aria-hidden', 'true');
   };
 
   return {
-    initializePins: function (pinsE, pinMap, dialogE, dialogClose) {
-
-      pinMap.addEventListener('click', function (evt) {
-        var targetPin = evt.target.parentNode;
-        if (targetPin.classList.contains('pin')) {
-          showDialog(evt.target.parentNode, pinsE, dialogE);
-        }
-      });
-
-      pinMap.addEventListener('keydown', function (evt) {
-        var targetPin = evt.target;
-        if (targetPin.classList.contains('pin') && isActivateEvent(evt)) {
-          showDialog(evt.target, pinsE, dialogE);
-        }
-      });
-
+    closeDialog: function (pinsE, dialogE, dialogClose, cb) {
       dialogClose.addEventListener('click', function () {
         hideDialog(pinsE, dialogE);
       });
 
       dialogClose.addEventListener('keydown', function (evt) {
-        if (isActivateEvent(evt)) {
-          hideDialog(pinsE, dialogE);
+        if (window.utils.isActivateEvent(evt)) {
+          hideDialog(pinsE, dialogE, cb);
         }
       });
     }
---
+++ js/utils.js
@@ -0,0 +1,18 @@
+'use strict';
+
+window.utils = (function () {
+  var ENTER_KEY_CODE = 13;
+
+  return {
+    isActivateEvent: function (evt) {
+      return evt.keyCode && evt.keyCode === ENTER_KEY_CODE;
+    },
+
+    removeClass: function (pinsElements, className) {
+      for (var j = 0; j < pinsElements.length; j++) {
+        pinsElements[j].classList.remove(className);
+        pinsElements[j].setAttribute('aria-pressed', 'false');
+      }
+    }
+  }
+})();
