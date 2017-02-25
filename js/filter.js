18e759a868b66b820cddc7bdf8b73e9d9af0bc45
Elizaveta <eljobemail@gmail.com>
2017-02-25 16:23:23 +0300

    filter.js v2

--- js/filter.js
+++ js/filter.js
@@ -2,12 +2,12 @@
 
 window.initializeFilter = (function () {
   var filterForm = document.querySelector('.tokyo__filters');
-  var i;
-  var j;
+  var ii;
+  var jj;
 
   var contains = function (where, what) {
-    for (var i = 0; i < what.length; i++) {
-      if (where.indexOf(what[i]) === -1) {
+    for (var ii = 0; ii < what.length; ii++) {
+      if (where.indexOf(what[ii]) === -1) {
         return false;
       }
     }
@@ -17,15 +17,15 @@ window.initializeFilter = (function () {
   var filter = function (filterObject, pinsArray) {
     var resultPinArray = [];
 
-    for (i = 0; i < pinsArray.length; i++) {
-      if (filterObject.type === pinsArray[i].offer.type || filterObject.type === 'any') {
-        if ((filterObject.price === 'middle' &&  (10000 <= pinsArray[i].offer.price <= 50000)) ||
-            (filterObject.price === 'low' && pinsArray[i].offer.price < 10000) ||
-            (filterObject.price === 'hight' && pinsArray[i].offer.price > 50000)) {
-          if (+filterObject.rooms === pinsArray[i].offer.rooms || filterObject.rooms === 'any') {
-            if (+filterObject.guests === pinsArray[i].offer.guests || filterObject.guests === 'any') {
-              if(contains(pinsArray[i].offer.features, filterObject.features)) {
-                resultPinArray.push(pinsArray[i]);
+    for (ii = 0; ii < pinsArray.length; ii++) {
+      if (filterObject.type === pinsArray[ii].offer.type || filterObject.type === 'any') {
+        if ((filterObject.price === 'middle' && ((pinsArray[ii].offer.price >= 10000) && (pinsArray[ii].offer.price <= 50000))) ||
+            (filterObject.price === 'low' && pinsArray[ii].offer.price < 10000) ||
+            (filterObject.price === 'hight' && pinsArray[ii].offer.price > 50000)) {
+          if (+filterObject.rooms === pinsArray[ii].offer.rooms || filterObject.rooms === 'any') {
+            if (+filterObject.guests === pinsArray[ii].offer.guests || filterObject.guests === 'any') {
+              if(contains(pinsArray[ii].offer.features, filterObject.features)) {
+                resultPinArray.push(pinsArray[ii]);
               }
             }
           }
@@ -42,22 +42,22 @@ window.initializeFilter = (function () {
       currentFilter.type = filterType.options[filterType.selectedIndex].value;
       var filterPrice = filterForm.elements.housing_price;
       currentFilter.price = filterPrice.options[filterPrice.selectedIndex].value;
-      var filterRooms = filterForm.elements[name = 'housing_room-number'];
+      var filterRooms = filterForm.elements['housing_room-number'];
       currentFilter.rooms = filterRooms.options[filterRooms.selectedIndex].value;
-      var filterGuests = filterForm.elements[name = 'housing_guests-number'];
+      var filterGuests = filterForm.elements['housing_guests-number'];
       currentFilter.guests = filterGuests.options[filterGuests.selectedIndex].value;
-      var filterFeatures = filterForm.querySelector("#housing_features").elements;
+      var filterFeatures = filterForm.querySelector('#housing_features').elements;
       currentFilter.features = [];
-      for (j = 0; j < filterFeatures.length; j++) {
-        if (filterFeatures[j].checked) {
-          currentFilter.features.push(filterFeatures[j].value);
+      for (jj = 0; jj < filterFeatures.length; jj++) {
+        if (filterFeatures[jj].checked) {
+          currentFilter.features.push(filterFeatures[jj].value);
         }
       }
       window.load('https://intensive-javascript-server-pedmyactpq.now.sh/keksobooking/data', function (data) {
         var pinMapEll = document.querySelector('.tokyo__pin-map');
         var removedPins = document.querySelectorAll('.pin:not(.pin__main)');
-        for (i = 0; i < removedPins.length; i++) {
-          pinMapEll.removeChild(removedPins[i]);
+        for (ii = 0; ii < removedPins.length; ii++) {
+          pinMapEll.removeChild(removedPins[ii]);
         }
         var filterData = filter(currentFilter, data);
         window.initializePins(pinMapEll, filterData, filterData.length);
